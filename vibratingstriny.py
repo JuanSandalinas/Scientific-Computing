6c641e9986bebe79597fbd1d435cf3e22941e946
JuanSandalinas <146134950+JuanSandalinas@users.noreply.github.com>
2024-02-12 18:11:38 +0100

    Update vibratingstriny.py

--- vibratingstriny.py
+++ vibratingstriny.py
@@ -9,17 +9,16 @@ import matplotlib.animation as animation
 from scipy.sparse import diags
 from scipy.sparse import csr_array
 
-PLOT = False
-ANIMATION = True
+PLOT = True
+ANIMATION = False
 
-STEP_PLOT = 500
-SAVE_ANIMATION = False
+STEP_PLOT = 1
+SAVE_ANIMATION = True
 
 N = 100
 TAO = 0.001
 TOTAL_TIME = 100
 
-SHIFT = 10**4
 
 def initial_cte(N,L = 1):
     """
@@ -32,7 +31,7 @@ def initial_cte(N,L = 1):
     return np.sin(2*np.pi*(x)),x
 
 
-def matrix_A(N, tao = TAO, L = 1):
+def matrix_A(N, tao = TAO, L = 1, c = 1):
     """
     Given a number of points, creates the matrix A of shape N,N
     Input:
@@ -41,11 +40,10 @@ def matrix_A(N, tao = TAO, L = 1):
     """
 
     h = L/N
-    r = (tao**2)/(h**2)
+    r = (c*tao/h)**2
     diagonals = [np.full(N-2, r),np.full(N-1, 2 - 2*r), np.full(N-2, r)]
     A = diags(diagonals , [-1, 0, 1])
     A = A.toarray()
-    A = A
     
     return A
 
@@ -55,48 +53,45 @@ def time_stepping(N,tao = TAO, steps = TOTAL_TIME/TAO, plot = True):
     """
     Does the stepping scheme over time
     """
-    y_ta = np.zeros(N+1)
-    y_to,x = initial_cte(N)
-    y_tb = y_to
+    y_new = np.zeros(N+1)
+    y,x = initial_cte(N)
+    y_old = np.copy(y)
     A = matrix_A(N)
-    time_step = 0
-    plt.plot(x,y_to, label = time_step)
+    plt.plot(x,y, label = time_step)
 
-    while True:
-        A_ = A@y_to[1:-1]*SHIFT
-        b = -y_tb[1:-1]*SHIFT
-        y_ta[1:-1] = (A_+b)/SHIFT
-        y_tb[1:-1] = y_to[1:-1]
-        y_to[1:-1] = y_ta[1:-1]
-        time_step += 1
+
+    for time_step in range(steps):
+
+        A_ = A@y[1:-1]
+        b = -y_old[1:-1]
+        y_new[1:-1] = (A_+b)
+        y_old[1:-1] = np.copy(y[1:-1])
+        y[1:-1] = np.copy(y_new[1:-1])
+
 
         if (time_step%STEP_PLOT) == 0:
-            plt.plot(x,y_to, label = f"Time = {0.001*time_step}")
-        if time_step >= steps:
-            break
-    plt.legend()
-    plt.show()
+            plt.plot(x,y, label = f"Time = {0.001*time_step}")
+       
+
 
 def step(interval):
-    global y_ta
-    global y_to
-    global y_tb
+    global y_new
+    global y
+    global y_old
     global x
 
     for _ in range(STEP_PLOT):
+        b = -y_old[1:-1]
+        y_new[1:-1] = A@y[1:-1] + b
 
-        A_ = A@(y_to[1:-1]*SHIFT)
-        b = -y_tb[1:-1]*SHIFT
-        y_ta[1:-1] = (A_+b)/SHIFT
+        y_old[1:-1] = np.copy(y[1:-1])
 
-        y_tb[1:-1] = y_to[1:-1]
-
-        y_to[1:-1] = y_ta[1:-1]
+        y[1:-1] = np.copy(y_new[1:-1])
     
     ax.clear()
     ax.set_ylim(-1.5,1.5)
-    ax.set_title(f'Step {interval*STEP_PLOT*TAO}')
-    ax.plot(x,y_to)
+    ax.set_title(f'Time_ {np.round(interval*STEP_PLOT*TAO,2)}')
+    ax.plot(x,y)
 
     
 if __name__ == "__main__":
@@ -106,18 +101,18 @@ if __name__ == "__main__":
 
     elif ANIMATION == True:
 
-
         fig, ax = plt.subplots()
 
-        y_ta = np.zeros(N+1)
-        y_to,x = initial_cte(N)
-        y_tb = y_to
-       
+        y_new = np.zeros(N+1)
+        y,x = initial_cte(N)
+        y_old =np.copy(y)       
         A = matrix_A(N)
-        ax.plot(x,y_to)
+        ax.plot(x,y)
 
-        anim = animation.FuncAnimation(fig,step, frames=int(10_000), interval=0.001, blit=False)
+        anim = animation.FuncAnimation(fig,step, frames=int(10_00), interval=0.001, blit=False)
+
         if SAVE_ANIMATION == True:
-             anim.save('wave_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-        plt.show()
+            anim.save('sin_wave_animation.mp4', fps=30)
+            plt.close()
+
 
