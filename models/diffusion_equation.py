c06eb469f549ca36d6cbbb27c3740dd7c1342629
JuanSandalinas <146134950+JuanSandalinas@users.noreply.github.com>
2024-02-20 12:27:27 +0100

    sss

--- models/diffusion_equation.py
+++ models/diffusion_equation.py
@@ -1,105 +1,119 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 from scipy.sparse import diags
-from scipy.sparse import csr_array
+from scipy.sparse import csr_matrix
+#from methods import jacobi
 
 
 class SimulationGrid:
-    def __init__(self, N):
+
+    def __init__(self, N, D = 1, objects = None):
+        """
+        Creates a simulation grid.
+        Inputs:
+            - N: How many intervals divide space
+            - D: Parameter , default set to 1
+            - object: Objects positions if there are any, default set to none
+        """
+        self.D = 1
         self.dx = 1/N
-        self.dt = None
-        self.initialize(N)
-        self.shape = self.current.shape
-    
-    def initialize_cte(self,N):
-        self.D = 1 
-        self.dt = (self.dx**2)/(4*self.D)
-        A = np.zeros((N+1, N+1))
-        A[0,:] = 1
-        self.current = A 
-
-    def time_step(self,function,stop):
-        for _ in range(self.n_steps):
-            self.c = function(self.c,stop)
-
-    def time_dependent():
-        A = self.current
-        dt = self.dt
-        dx = self.dx
-        term = (self.dt*self.D)/(self.dx**2)
-        current_time = 0
-        data = [A]
-        times = np.arange(0,tmax+dt, dt)
-        Ax = A.copy()
-        nrows, ncols = A.shape
-        sampling_timer = [None]
-        sampling_times = []
-        
-        for iteration in times: 
-            A0 = data[-1] 
-            for k in range(1, nrows-1):
-                for j in range(1, ncols-1):
-                    c1 = A0[k-1, j]
-                    c2 = A0[k+1, j]
-                    c3 = A0[k,j-1]
-                    c4 = A0[k,j+1]
-                    c0 = A0[k, j]
-                    Ax[k,j] = c0 + term*(c1 + c2 + c3 + c4 - (4*c0))
-            Ax[:,0] = Ax[:,1]
-            Ax[:,-1] = Ax[:,-2]
-            sampling_timer.pop()
-            
-            if len(sampling_timer) < 1:
-                data.append(Ax.copy())
-                sampling_times.append(iteration)
-                sampling_timer = [k for k in range(frequency)]
-        
-        return data, np.array(sampling_times)
-
-        def c_analytical(y, t, D=1, imax=100):
-            """
-            Returns analytical solution of diffusion equation
-            Inputs:
-                - y: y axis solution
-                - t: time steps
-            """
-
-            lst = []
-            for y_i in y:
-                result = 0
-                for i in range(100):
-                    arg1 = math.erfc((1 - y_i + (2*i)) /(2 * np.sqrt(D )* t))
-                    arg2 = math.erfc((1 + y_i + (2*i)) /(2 * np.sqrt(D )* t))
-                    result += (arg1-arg2)
-                lst.append(result)
-            return np.array(lst)
-
-    
-        
-"""
-class Diffusion():
-
-    def __init__(self, N, T,L = 1, tao = 0.001,auto = True):
         self.N = N
-        self.T = T
-        self.tao = tao
+        self.initialize()
 
-        self.n_steps = int(T/tao)
-        self.x = np.linspace(0,L,self.N+1)
-        self.y = np.linspace(0,L, self.N+1)
 
-        if auto == True:
-            self.initial_cte()
+    def initialize(self):
+        """
+        Initializes matrix with all all 0 concentrations except 1 on first row
+        """
+        A = np.zeros((self.N+1, self.N+1))
+        A[0,:] = 1
+        self.A = A
 
-    def initial_cte(self):
-        self.c_o = np.zeros((self.N+1,self.N+1))
-        self.c_o[0][1:-1] = np.ones(self.N+1)
-        self.c = self.c_o
 
-    def time_step(self,function,stop):
-        for _ in range(self.n_steps):
-            self.c = function(self.c,stop)
-"""
+    def time_independent(self,method_fun,*args,**kwargs):
+        """
+        Executes a time_step ing method given a function
+        Inputs:
+            - method_func: Method to use function
+            - stop: Stopping criteria
+            - store_step: Every how many steps store data
+        """
+        self.data = [self.A]
+        A = np.copy(self.A)
+        for A_t in method_fun(A,*args,**kwargs):
+            self.data.append(A_t)
+
+
+    
+    def time_dependent(self,t,dt = 0.0001, time_list = [0,0.001,0.01,0.1,1.0]):
+        """
+        Does time dependent stepping scheme.
+        Inputs:
+            - t: total time
+            - dt: Time step in seconds
+            - time_list: times to store
+        """
+        self.data = [self.A]
+        term = (dt*self.D)/(self.dx**2)
+
+        if 4*term > 1:
+            raise Exception ("Not stable system")
+
+        C = np.copy(self.A)
+        n_steps = int(t/dt)
+
+        ## Matrix operations
+        diagM1 = [term*np.ones(self.N),np.ones(self.N+1),term*np.ones(self.N)]
+        diagM2 = [term*np.ones(self.N),np.full(self.N+1,-4*term),term*np.ones(self.N)]
+
+        M1 = diags(diagM1 , [-1, 0,1], format = "csr")
+        M2 = diags(diagM2, [-1,0,1], format = "csr")
+
+        ## Periodic boundary conditions
+        M2[-2,0] = 1
+        M2[1,-1] = 1
+
+
+        for k in range(n_steps):
+            c1=  M1@C
+            c2 = C@M2
+            C[1:-1] = (c1+c2)[1:-1]
+            if k*dt in time_list:
+                print(k*dt)
+                self.data.append(C)
+
+
+    def c_analytical(self,t, i_max=100):
+        """
+        Returns analytical solution of diffusion equation
+        Inputs:
+            - y: y axis solution
+            - t: time period
+        """
+        lst = []
+        y = np.linspace(0,1,self.N)
+        for y_i in y:
+            result = 0
+            for i in range(i_max):
+                arg1 = math.erfc((1 - y_i + (2*i)) /(2 * np.sqrt(self.D*t)))
+                arg2 = math.erfc((1 + y_i + (2*i)) /(2 * np.sqrt(self.D*t)))
+                result += (arg1-arg2)
+            lst.append(result)
+        return np.array(lst)
+
+
+ 
 if __name__ == "__main__":
-    dif = Diffusion(4,1)
-    dif.time_step(gauss_seidel,0.01)
-    print(dif.c)
+    dif = SimulationGrid(50)
+    y_input = np.arange(0,1,51)
+    time_show = [0,0.001, 0.01, 0.1, 1] 
+    dt = 0.00001
+    dif.time_dependent(t = 1, dt = dt,time_list = time_show)
+    for i,d in enumerate(dif.data):
+        text = f"Time dependent - t = {time_show[i]}"
+        plt.plot(d[::-1,1],label=text)
+        plt.legend()
+    plt.show()
+
+    
--- models/methods.py
+++ models/methods.py
@@ -6,32 +6,45 @@ from scipy import special as sp
 import math
 
 
-
-def jacobi(C, stop):
+def jacobi(C, stop,store_step = 10, object = None):
     """
-    Jacobi method:
+    Jacobi method.
     Inputs:
         - C: Matrix with initial conditions
         - Stop: Stopping criteria
+        - Boundary: Boundary conditions that wont change, default top and last row
+        - Object: If there is any object, default to 0 and -1 rows
     """
     w = 1/4
-    n_rows = A.shape[0]
-    n_cols = A.shape[1]
-    diagonals = [np.ones(ncol-1),np.ones(ncol), np.ones(ncols-1)]
-    M1 = diags(diagonals , [-1, 0, 1])
-    M2 = diags([diagonals[0], diagonals[2]], [-1,1]).toarray()
+    n_cols = C.shape[1]
+
+    diagonals = [np.ones(n_cols-1), np.ones(n_cols-1)]
+    
+    M1 = diags(diagonals , [-1, 1])
+    M2 = diags(diagonals, [-1,1]).toarray()
+
+    ## Cyclic boundary conditions
     M2[-2,0] = 1
     M2[1,-1] = 1
+
+    n_count = 0
+
     while True:
-        C_d = C
-        C_ = 1/4 * M1@C@M2
-        C[1:-1] = C_[1:-1]
-        if np.abs(C-C_b).all() <= stop:
+        n_count += 1
+        C_b = np.copy(C)
+        c1=  M1@C
+        c2 = C@M2
+        C[1:-1] = (1/4 *(c1+c2))[1:-1]
+
+        if np.allclose(C, C_b, atol=stop):
+            yield C
             break
-    return C
+        if n_count%store_step == 0:
+            yield C
+    
 
 
-def gauss_seidel(C, stop):
+def gauss_seidel(C, stop,store_step = 10, object = None):
     """
     Performs Gauss Seidel iterations
     Inputs:
@@ -39,21 +52,24 @@ def gauss_seidel(C, stop):
         - Stop: Stop criteria
     """
     w = 1/4
-    C_b = np.ones((C.shape[0], C.shape[1]))
+    n_count = 0
     while True:
-        for i in range(0,A.shape[0]):
-            C[i,-1] = c*(C[i+1,j] + C[i-1,j] + C[i,j+1] + C[i,-1])
-            for j in range(1,A.shape[1]-1):
+        n_count += 1 
+        C_b = np.copy(C)
+        for i in range(1,C.shape[0]-1):
+            C[i,0] = w*(C[i+1,0] + C[i-1,0] + C[i,1] + C[i,-2])
+            for j in range(1,C.shape[1]-1):
                 C[i,j] = w*(C[i+1,j] + C[i-1,j] + C[i,j+1] + C[i,j-1])
+            C[i,-1] = w*(C[i+1,-1] + C[i-1,-1] + C[i,1] + C[i,-2])
 
-            C[i,-1] = C[i,0]
-        if np.abs(C-C_b).all() <= stop:
+        if np.allclose(C, C_b, atol=stop):
+            yield C
             break
-        C_b = C
+        if n_count%store_step == 0:
+            yield C
 
-    return C
 
-def sor(C,w,stop):
+def sor(C,w,stop = 0.001,store_step = 10, object = None):
     """
     Performs Successive over relaxation.
     Inputs:
@@ -61,37 +77,22 @@ def sor(C,w,stop):
         - w: weight
         - stop: simulation stopper
     """
-    
-    C_b = np.ones((C.shape[0], C.shape[1]))
+    n_count = 0
     while True:
+        n_count += 1
+        C_b = np.copy(C)
         for i in range(1,C.shape[0]-1):
-            for j in C.size[1]:
-                C_k[i,j] = w*(C_k[i+1,j] + C_k[i-1,j] + C_k[i,j+1] + C_k[i,j-1]) + (1-w)*C_k[i,j]
-            C[i,-1] = C[i,0]
-
-        if np.abs(C-C_b).all() <= stop:
+            C[i,0] = w*(C[i+1,0] + C[i-1,0] + C[i,1] + C[i,-2]) + (1-w)*C[i,0]
+            for j in range(1,C.shape[1]-1):
+                C[i,j] = (w/4)*(C[i+1,j] + C[i-1,j] + C[i,j+1] + C[i,j-1]) + (1-w)*C[i,j]
+            C[i,-1] = w*(C[i+1,-1] + C[i-1,-1] + C[i,1] + C[i,-2]) + (1-w)*C[i,-1]
+        
+        if np.allclose(C, C_b, atol=stop):
+            yield C
             break
-        A_b = A
-    return A
-
-
-def c_analytical(y, t, D=1, imax=100):
-    """
-    Returns analytical solution of diffusion equation
-    Inputs:
-        - y: y axis solution
-        - t: time steps
-    """
-
-    lst = []
-     
-    result = 0
-    for i in range(100):
-        arg1 = math.erfc((1 - y_i + (2*i)) /(2 * np.sqrt(D)* t))
-        arg2 = math.erfc((1 + y_i + (2*i)) /(2 * np.sqrt(D)* t))
-        result += (arg1-arg2)
-        lst.append(result)
-    return np.array(lst)
+        if n_count%store_step == 0:
+            yield C
+    
 
 
 
