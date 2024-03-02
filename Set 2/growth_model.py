4eebd9a42001b826d562c9a584f5c65dd13a2edc
Axegky <132951747+Axegky@users.noreply.github.com>
2024-03-02 02:25:32 +0100

    Update growth_model.py

--- Set 2/growth_model.py
+++ Set 2/growth_model.py
@@ -1,24 +1,30 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import random
 
 def growth_model(N, position, w, eta, stop):
+    # N is the grid size
+    # position is the initial place with a sink
+
     if (position[0]) >= (N - 1) or (position[1]) >= (N - 1):
         raise Exception("Outside bounds")
 
+    # C is the 3-dimensional matrix for iteration
     C = np.zeros((N + 1, N + 1, N + 1))
 
+    # Initial condition
     C[0][0][:] = 1
 
     n_count = 0
 
+    # cluster is the 3-dimensional matrix for updating the sinks
     cluster = np.zeros((N + 1, N + 1, N + 1))
 
+    # initial condition for the sink
     cluster[0][position[0]][position[1]] = 1
-    C[0][position[0]][position[1]] = 1
 
     measure = 1
 
+    # SOR
     while measure > stop:
 
         n_count += 1
@@ -31,27 +37,19 @@ def growth_model(N, position, w, eta, stop):
                 pass
                 measure_n = measure
             elif non_cte[1][i] == 0:
-                C[n_count][non_cte[0][i]][non_cte[1][i]] = w * 0.25 * (
-                            C[n_count][non_cte[0][i] - 1][0] + C[n_count - 1][non_cte[0][i] + 1][0] +
-                            C[n_count - 1][non_cte[0][i]][2] + C[n_count][non_cte[0][i]][non_cte[1][-1]]) + (1 - w) * \
-                                                           C[n_count - 1][non_cte[0][i]][0]
+                C[n_count][non_cte[0][i]][non_cte[1][i]] = w * 0.25 * (C[n_count][non_cte[0][i] - 1][0] + C[n_count - 1][non_cte[0][i] + 1][0] + C[n_count - 1][non_cte[0][i]][2] + C[n_count][non_cte[0][i]][non_cte[1][-1]]) + (1 - w) * C[n_count - 1][non_cte[0][i]][0]
                 measure_n = C[n_count][non_cte[0][i]][0] - C[n_count - 1][non_cte[0][i]][non_cte[1][i]]
             elif non_cte[1][i] == N:
-                C[n_count][non_cte[0][i]][N] = w * 0.25 * (
-                            C[n_count][non_cte[0][i] - 1][N] + C[n_count - 1][non_cte[0][i] + 1][N] +
-                            C[n_count][non_cte[0][i]][N + 1] + C[n_count - 1][non_cte[0][i]][N - 1]) + (1 - w) * \
-                                               C[n_count - 1][non_cte[0][i]][N]
+                C[n_count][non_cte[0][i]][N] = w * 0.25 * (C[n_count][non_cte[0][i] - 1][N] + C[n_count - 1][non_cte[0][i] + 1][N] + C[n_count][non_cte[0][i]][N + 1] + C[n_count - 1][non_cte[0][i]][N - 1]) + (1 - w) * C[n_count - 1][non_cte[0][i]][N]
                 measure_n = C[n_count][non_cte[0][i]][N] - C[n_count - 1][non_cte[0][i]][N]
             else:
-                C[n_count][non_cte[0][i]][non_cte[1][i]] = w * 0.25 * (
-                            C[n_count][non_cte[0][i] - 1][non_cte[1][i]] + C[n_count - 1][non_cte[0][i] + 1][
-                        non_cte[1][i]] + C[n_count - 1][non_cte[0][i]][non_cte[1][i] + 1] + C[n_count][non_cte[0][i]][
-                                non_cte[1][i] - 1]) + (1 - w) * C[n_count - 1][non_cte[0][i]][non_cte[1][i]]
+                C[n_count][non_cte[0][i]][non_cte[1][i]] = w * 0.25 * (C[n_count][non_cte[0][i] - 1][non_cte[1][i]] + C[n_count - 1][non_cte[0][i] + 1][non_cte[1][i]] + C[n_count - 1][non_cte[0][i]][non_cte[1][i] + 1] + C[n_count][non_cte[0][i]][non_cte[1][i] - 1]) + (1 - w) * C[n_count - 1][non_cte[0][i]][non_cte[1][i]]
                 measure_n = C[n_count][non_cte[0][i]][non_cte[1][i]] - C[n_count - 1][non_cte[0][i]][non_cte[1][i]]
 
             if measure_n > measure:
                 measure = measure_n
 
+        # changing the concentration of sinks into 0
         for i in range(N):
             for j in range(N):
                 if cluster[n_count][i][j] == 1:
@@ -63,6 +61,7 @@ def growth_model(N, position, w, eta, stop):
             a = sink[0][k]
             b = sink[1][k]
 
+            # recording the concentration of four candidates
             if a == 0 and b == 0:
                 u = 0
                 d = C[n_count][a + 1][b] ** eta
@@ -109,19 +108,7 @@ def growth_model(N, position, w, eta, stop):
                 l = C[n_count][a][b - 1] ** eta
                 r = C[n_count][a][b + 1] ** eta
 
-            if a != 0:
-                if cluster[n_count][a - 1][b] == 1:
-                    u = 0
-            if a != N:
-                if cluster[n_count][a + 1][b] == 1:
-                    d = 0
-            if b != 0:
-                if cluster[n_count][a][b - 1] == 1:
-                    l = 0
-            if b != N:
-                if cluster[n_count][a][b + 1] == 1:
-                    r = 0
-
+            # calculating the probability of becoming a sink for candidates
             if (d + u + l + r) != 0:
                 for i in [a - 1, a + 1]:
                     for j in [b - 1, b + 1]:
